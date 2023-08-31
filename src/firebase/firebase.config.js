b030669e06dc054fd8baec5848bded2cf527accc
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-31 14:33:28 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8erVlpoKje5YN_aMDyla4kyMYqzZZZ2Y",
+  authDomain: "daily-news-29016.firebaseapp.com",
+  projectId: "daily-news-29016",
+  storageBucket: "daily-news-29016.appspot.com",
+  messagingSenderId: "167157116299",
+  appId: "1:167157116299:web:8a346d3c697354da7ed38e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
